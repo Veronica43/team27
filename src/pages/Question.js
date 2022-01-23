b9dbdf6bfb79fe9deea762ce486999ff52db7409
Veronica Ortega <v.ortegaferreres@gmail.com>
2022-01-23 15:43:10 +0100

    new components

--- src/pages/Question.js
+++ src/pages/Question.js
@@ -1,106 +1,10 @@
 import React from "react";
-import { useState } from "react";
-
-import Timer from "../components/Timer";
-import Trivia from "../components/Trivia";
+import { AnswerQuestion } from "../components/AnswerQuestion";
 
 export const Question = () => {
-  const [timeOut, setTimeOut] = useState(false);
-  const [questionNumber, setQuestionNumber] = useState(1);
-
-  const data = [
-    {
-      id: 1,
-      question: "Rolex is a company that specializes in what type of product?",
-      answers: [
-        {
-          text: "Phone",
-          correct: false,
-        },
-        {
-          text: "Watches",
-          correct: true,
-        },
-        {
-          text: "Food",
-          correct: false,
-        },
-        {
-          text: "Cosmetic",
-          correct: false,
-        },
-      ],
-    },
-    {
-      id: 2,
-      question: "When did the website `Facebook` launch?",
-      answers: [
-        {
-          text: "2004",
-          correct: true,
-        },
-        {
-          text: "2005",
-          correct: false,
-        },
-        {
-          text: "2006",
-          correct: false,
-        },
-        {
-          text: "2007",
-          correct: false,
-        },
-      ],
-    },
-    {
-      id: 3,
-      question: "Who played the character of harry potter in movie?",
-      answers: [
-        {
-          text: "Johnny Deep",
-          correct: false,
-        },
-        {
-          text: "Leonardo Di Caprio",
-          correct: false,
-        },
-        {
-          text: "Denzel Washington",
-          correct: false,
-        },
-        {
-          text: "Daniel Red Cliff",
-          correct: true,
-        },
-      ],
-    },
-  ];
-
   return (
-    <div className="app">
-      <>
-        <div className="main">
-          <>
-            <div className="top">
-              <div className="timer">
-                <Timer
-                  setTimeOut={setTimeOut}
-                  questionNumber={questionNumber}
-                />
-              </div>
-            </div>
-            <div className="bottom">
-              <Trivia
-                data={data}
-                questionNumber={questionNumber}
-                setQuestionNumber={setQuestionNumber}
-                setTimeOut={setTimeOut}
-              />
-            </div>
-          </>
-        </div>
-      </>
+    <div>
+      <AnswerQuestion />
     </div>
   );
 };
