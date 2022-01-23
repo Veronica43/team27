037d9bc8a14436068925c45a4c5eaa79cf8c388e
Veronica Ortega <v.ortegaferreres@gmail.com>
2022-01-23 13:59:59 +0100

    data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-
-
-import './styles/App.scss';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Register } from './pages/Register';
-import { Game } from './pages/Game';
+import "./styles/App.scss";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Register } from "./pages/Register";
+import { Game } from "./pages/Game";
 import { Question } from "./pages/Question";
-
-
 
 function App() {
   return (
@@ -15,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Register />} />
           <Route path="/game" element={<Game />} />
+          <Route path="/question" element={<Question />} />
         </Routes>
       </BrowserRouter>
     </div>
--- src/pages/Question.js
+++ src/pages/Question.js
@@ -1,14 +1,106 @@
 import React from "react";
-import { useEffect, useState } from "react";
-import useSound from "use-sound";
-import play from "../sounds/play.mp3";
-import correct from "../sounds/correct.mp3";
-import wrong from "../sounds/wrong.mp3";
+import { useState } from "react";
+
+import Timer from "../components/Timer";
+import Trivia from "../components/Trivia";
 
 export const Question = () => {
-  const [letsPlay] = useSound(play);
-  const [correctAnswer] = useSound(correct);
-  const [wrongAnswer] = useSound(wrong);
+  const [timeOut, setTimeOut] = useState(false);
+  const [questionNumber, setQuestionNumber] = useState(1);
 
-  return <div></div>;
+  const data = [
+    {
+      id: 1,
+      question: "Rolex is a company that specializes in what type of product?",
+      answers: [
+        {
+          text: "Phone",
+          correct: false,
+        },
+        {
+          text: "Watches",
+          correct: true,
+        },
+        {
+          text: "Food",
+          correct: false,
+        },
+        {
+          text: "Cosmetic",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 2,
+      question: "When did the website `Facebook` launch?",
+      answers: [
+        {
+          text: "2004",
+          correct: true,
+        },
+        {
+          text: "2005",
+          correct: false,
+        },
+        {
+          text: "2006",
+          correct: false,
+        },
+        {
+          text: "2007",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 3,
+      question: "Who played the character of harry potter in movie?",
+      answers: [
+        {
+          text: "Johnny Deep",
+          correct: false,
+        },
+        {
+          text: "Leonardo Di Caprio",
+          correct: false,
+        },
+        {
+          text: "Denzel Washington",
+          correct: false,
+        },
+        {
+          text: "Daniel Red Cliff",
+          correct: true,
+        },
+      ],
+    },
+  ];
+
+  return (
+    <div className="app">
+      <>
+        <div className="main">
+          <>
+            <div className="top">
+              <div className="timer">
+                <Timer
+                  setTimeOut={setTimeOut}
+                  questionNumber={questionNumber}
+                />
+              </div>
+            </div>
+            <div className="bottom">
+              <Trivia
+                data={data}
+                questionNumber={questionNumber}
+                setQuestionNumber={setQuestionNumber}
+                setTimeOut={setTimeOut}
+              />
+            </div>
+          </>
+        </div>
+      </>
+    </div>
+  );
 };
