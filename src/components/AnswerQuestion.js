558c22508f048128dea60f33c51b4684b8e1052f
MinervaLong <vanesa.perea.cuevas@gmail.com>
2022-01-23 18:37:26 +0100

    Add random questions

--- src/components/AnswerQuestion.js
+++ src/components/AnswerQuestion.js
@@ -2,15 +2,32 @@ import React from "react";
 import { Button } from "./Button";
 import '../styles/AnswerQuestion.scss';
 
-export const AnswerQuestion = ({ question, points, setPoints }) => {
+
+
+export const AnswerQuestion = ({ question, points, setPoints, setIndex, index }) => {
+  /*const handleClick = (answer) => {
+    console.log(answer)
+    if(answer.isTrue){
+      setPoints(points + 10)
+     
+    }
+    setIndex(index + 1)
+  
+  }*/
   return (
     <div className="question-container">
       <p className="question">{question.question}</p>
-      {question.answers.map((ansewer, index) => (
+      {question.answers.map((answer, index) => (
         <Button
           key={index}
-          text={ansewer.option}
-          method={() => (ansewer.isTrue ? setPoints(points + 10) : 0)}
+          text={answer.option}
+          method={() => {
+            if(answer.isTrue){
+              setPoints(points + 10)             
+            }
+            setIndex(index + 1)
+          }}
+          
         />
       ))}
     </div>
