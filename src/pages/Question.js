3d6e5d3ebe5183ef80310c7788d5238f44484af5
Veronica Ortega <v.ortegaferreres@gmail.com>
2022-01-23 12:58:29 +0100

    questionPage

--- src/pages/Question.js
+++ src/pages/Question.js
@@ -1,5 +1,14 @@
 import React from "react";
+import { useEffect, useState } from "react";
+import useSound from "use-sound";
+import play from "../sounds/play.mp3";
+import correct from "../sounds/correct.mp3";
+import wrong from "../sounds/wrong.mp3";
 
 export const Question = () => {
+  const [letsPlay] = useSound(play);
+  const [correctAnswer] = useSound(correct);
+  const [wrongAnswer] = useSound(wrong);
+
   return <div></div>;
 };
