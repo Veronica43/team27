09169c74afc9abfd446d33df0808f588a923094f
sarah <sschmidt0@uoc.edu>
2022-01-23 11:04:08 +0100

    example data structure

--- src/assets/questions.js
+++ src/assets/questions.js
@@ -1,3 +1,36 @@
 const questions = [
-  {},
+  {
+    question: 'sdfkajsdlfkjs?',
+    anwsers: [
+      { option: 'answer 1',
+        isTrue: false,
+      },
+      { option: 'answer 2',
+        isTrue: true,
+      },
+      { option: 'answer 3',
+        isTrue: false,
+      },
+      { option: 'answer 4',
+        isTrue: false,
+      }
+    ],
+  },
+  {
+    question: 'sdfkajsdlfkjs?',
+    anwsers: [
+      { option: 'answer 1',
+        isTrue: false,
+      },
+      { option: 'answer 2',
+        isTrue: true,
+      },
+      { option: 'answer 3',
+        isTrue: false,
+      },
+      { option: 'answer 4',
+        isTrue: false,
+      }
+    ],
+  },
 ]
